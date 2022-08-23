9957de374947ea53be1920d0ec0209ea21c0cc84
gkdfo40 <gkdfo40@gmail.com>
2022-08-23 14:48:18 +0900

    feat(searchPage): Add style code

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react';
+import styles from './searchPage.module.scss';
 
 const SearchPage = () => {
   const [result, setResult] = useState('Repogitory');
   return (
-    <div>
-      <header>Find Repository</header>
+    <div className={styles.container}>
+      <header>
+        <h1>Find GitHub Repogitory</h1>
+      </header>
 
-      <form>
-        <div>
+      <form className={styles.searchForm}>
+        <div className={styles.inputBox}>
           {/* 돋보기 이미지 */}
           <input type="text" placeholder="typing..." />
         </div>
-        <select>
+        <select className={styles.optionBox}>
           <option selected>language</option>
           <option value="C">C</option>
           <option value="C++">C++</option>
@@ -23,19 +26,21 @@ const SearchPage = () => {
           <option value="PHP">PHP</option>
           <option value="Kotlin">Kotlin</option>
         </select>
-        <button type="submit">Search</button>
+        <button className={styles.submitbutton} type="submit">
+          Search
+        </button>
       </form>
-      <h1>{`Results for "${result}"`}</h1>
-      <section>
+      <h2>{`Results for "${result}"`}</h2>
+      <section className={styles.filterContainer}>
         <p>results found</p>
-        <select>
+        <select className={styles.filterBox}>
           <option value="Recently Updated" selected>
             Recently Updated
           </option>
           <option value="Best Match">Best Match</option>
         </select>
       </section>
-      <ul>
+      <ul className={styles.repositoriesContainer}>
         <li>저장소</li>
       </ul>
       <div>pageNation</div>
